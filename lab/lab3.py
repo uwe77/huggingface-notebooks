dbdba739bf2fe60f72ac0e21d3193ccc9a0e9690
uwe <uwe90711@gmail.com>
2024-03-21 14:53:08 +0800

    lab3

---
+++ lab/lab3.py
@@ -0,0 +1,15 @@
+from datasets import load_dataset
+from transformers import AutoTokenizer, DataCollatorWithPadding
+
+raw_datasets = load_dataset("glue", "mrpc")
+checkpoint = "bert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+
+def tokenize_function(example):
+    return tokenizer(example["sentence1"], example["sentence2"], truncation=True)
+
+
+tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+print(tokenized_datasets["train"][0])
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
