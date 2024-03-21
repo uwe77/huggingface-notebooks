bbf949b9c1349a24d56b814d52ce7e3443aa7d39
hchengwang <hchengwang@gmail.com>
2024-03-21 15:53:35 +0800

    add docstring and format 80 char per line

--- course/en/chapter3/finetune_transformer/download.py
+++ course/en/chapter3/finetune_transformer/download.py
@@ -2,7 +2,26 @@ from datasets import load_dataset
 from transformers import AutoTokenizer
 import os
 
-def download_dataset_and_tokenizer(checkpoint, dataset_name="glue", dataset_config="mrpc", tokenizer_name=None):
+def download_dataset_and_tokenizer(
+        checkpoint, 
+        dataset_name="glue", 
+        dataset_config="mrpc", 
+        tokenizer_name=None):
+    """
+    Download the dataset and tokenizer for a given checkpoint.
+    If the dataset or tokenizer already exists locally, it will not be downloaded again.
+
+    Args:
+    - checkpoint: The name of the checkpoint to use.
+    - dataset_name: The name of the dataset to download.
+    - dataset_config: The configuration of the dataset to download.
+    - tokenizer_name: The name of the tokenizer to download.
+
+    Returns:
+    - None
+
+    """
+
     tokenizer_name = tokenizer_name if tokenizer_name else checkpoint
     
     # Check and download the dataset
